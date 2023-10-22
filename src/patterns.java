ccfef192838835ccf9a65b7b442bbe96ac5737a0
sufyansidd19 <112391971+sufyansidd19@users.noreply.github.com>
2023-10-22 11:33:52 +0500

    double

--- src/patterns.java
+++ src/patterns.java
@@ -162,14 +162,15 @@ public class patterns{
     } 
 
     public static void pattern30(int n){
-        for(int row=1;row<=n;row++){
-            for(int space=0;space<n-row;space++){
+        for(int row=1;row<=2*n;row++){
+            int c= row>n ? 2*n - row : row;
+            for(int space=0;space<n-c;space++){
                 System.out.print(" ");
             }
-            for(int col=row;col>=1;col--){
+            for(int col=c;col>=1;col--){
                 System.out.print(col);
             }
-            for(int col=2;col<=row;col++){
+            for(int col=2;col<=c;col++){
                 System.out.print(col);
             }
             System.out.println();
