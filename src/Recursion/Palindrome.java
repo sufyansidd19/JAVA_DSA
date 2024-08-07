91c725318fccc224105e51139be34553feb57534
Muhammad Sufyan Siddiqui <112391971+sufyansidd19@users.noreply.github.com>
2024-08-07 16:46:40 +0500

    Practice Questions of Recursion

--- src/Recursion/Palindrome.java
+++ src/Recursion/Palindrome.java
@@ -1,7 +1,7 @@
 package Recursion;
 
-public class Reverse {
-//    Method no 1
+public class Palindrome {
+    //    Method no 1
     static int sum=0;
     static void Rev1(int n){
         if (n==0){
@@ -11,7 +11,7 @@ public class Reverse {
         sum= sum * 10 + rem;
         Rev1(n/10);
     }
-//    Method no 2
+    //    Method no 2
 //    if we don't want to use the outside variable
     static int Rev2(int n){
         int digits=(int)(Math.log10(n))+1;
@@ -24,10 +24,13 @@ public class Reverse {
         int rem =n%10;
         return rem*(int)(Math.pow(10,digits-1))+helper(n/10,digits-1);
     }
-
+    static boolean palin(int n){
+        return n==Rev2(n);
+    }
     public static void main(String[] args) {
 //        Rev1(9754);
-        System.out.println(Rev2(9754));
+//        System.out.println(Rev2(9754));
+        System.out.println(palin(123321));
 //        System.out.println(sum);
     }
 
