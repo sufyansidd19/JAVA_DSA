9846ecb0a46645b2c0c706780742858095dead25
Muhammad Sufyan Siddiqui <112391971+sufyansidd19@users.noreply.github.com>
2024-08-19 06:09:06 -0700

    LeetCode

--- src/LinkedList/LL.java
+++ src/LinkedList/LL.java
@@ -10,16 +10,24 @@ public class LL {
 
     public static void main(String[] args) {
         LL first=new LL();
+        LL second=new LL();
 //        LL second=new LL();
         first.insertLast(1);
         first.insertLast(1);
         first.insertLast(2);
-        first.insertLast(3);
-        first.insertLast(3);
-        first.display();
-        first.duplicates();
-        first.display();
+        first.insertLast(4);
+        first.insertLast(5);
+        second.insertLast(1);
+        second.insertLast(3);
+        second.insertLast(6);
+        second.insertLast(6);
+        second.insertLast(7);
+//        first.display();
+//        first.duplicates();
+//        first.display();
 
+        LL ans = LL.merge(first , second);
+        ans.display();
     }
 
     public void insertFirst(int val){
@@ -107,7 +115,30 @@ public class LL {
             tail=node;
             tail.next=null;
         }
-
+    public static LL merge(LL first , LL second){
+        Node f =first.head;
+        Node s =second.head;
+        LL ans=new LL();
+        while(f!=null && s!=null){
+            if(f.value<s.value){
+                ans.insertLast(f.value);
+                f=f.next;
+            }
+            else{
+                ans.insertLast(s.value);
+                s=s.next;
+            }
+        }
+        while(f!=null){
+            ans.insertLast(f.value);
+            f=f.next;
+        }
+        while(s!=null){
+            ans.insertLast(s.value);
+            s=s.next;
+        }
+        return ans;
+    }
 
 
     private class Node{
